065ca2cafe717d76c5d8915eac9da4ded51d2ecc
miracle3070 <miracle3070@naver.com>
2021-02-14 04:18:09 +0900

    Implemented Post Model

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to="image/", null=True, blank=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.content[:50]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return render(request, "index.html")
+    posts = Post.objects
+    return render(request, "index.html", {"posts" : posts})
