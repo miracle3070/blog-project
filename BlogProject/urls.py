22373cbc09c9a0e135160606676b6a4be00295c3
miracle3070 <miracle3070@naver.com>
2021-02-15 04:43:42 +0900

    Implemented media(image) file uploading

--- BlogProject/urls.py
+++ BlogProject/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 import blogapp
 import blogapp.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.index, name="index"),
     path('blog/', include('blogapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
