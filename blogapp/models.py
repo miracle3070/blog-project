dde72f4bd83d06e3014d099661dfaa05a3af3713
miracle3070 <miracle3070@naver.com>
2021-02-15 07:48:48 +0900

    Added author info in Post Model

--- blogapp/models.py
+++ blogapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
+    author = models.CharField(max_length=50, default="Unknown")
     image = models.ImageField(upload_to="image/", null=True, blank=True)
     content = models.TextField()
 
